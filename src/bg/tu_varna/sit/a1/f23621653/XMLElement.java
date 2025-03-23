9a5bcb361e819625393384319266d0c85560d34b
olympus-g <151548861+olympus-g@users.noreply.github.com>
2025-03-23 22:57:45 +0200

    Added fields and methods in the XMLElement class.

--- src/bg/tu_varna/sit/a1/f23621653/XMLElement.java
+++ src/bg/tu_varna/sit/a1/f23621653/XMLElement.java
@@ -1,5 +1,53 @@
 package bg.tu_varna.sit.a1.f23621653;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class XMLElement {
     //will be used to represent a single xml element
+
+    private String id;
+    private Map<String, String> attributes = new HashMap<>();
+    private List<XMLElement> children = new ArrayList<>();
+    private String text = "";
+
+    public XMLElement(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAttribute(String key) {
+        return attributes.getOrDefault(key, null);
+    }
+
+    public void setAttribute(String key, String value) {
+        attributes.put(key, value);
+    }
+
+    public void removeAttribute(String key) {
+        attributes.remove(key);
+    }
+
+    public void addChild(XMLElement child){
+        children.add(child);
+    }
+
+    public List<XMLElement> getChildren(){
+        return children;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    //add method for formatting later, maybe???
 }
